76f43263457fe61372a91026779a7853d680d553
DamjanJ <damjanjelic@gmail.com>
2017-12-07 22:02:48 +0100

    Commit 3, GPS

--- app/src/main/java/rs/org/habiprot/alciphron/alci/CameraActivity.java
+++ app/src/main/java/rs/org/habiprot/alciphron/alci/CameraActivity.java
@@ -23,6 +23,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
@@ -56,8 +58,17 @@ import static java.security.AccessController.getContext;
 
 public class CameraActivity extends AppCompatActivity {
 
-    LocationManager mLocationManager;
-    TextView tv5;
+    private LocationManager mLocationManager;
+    private TextView tv5;
+    private Location mlocation;
+    private FloatingActionButton fab;
+    private LocationListener mlocationListener;
+    private LocationManager locationManager;
+
+    private int Zone = 0;
+    private char Letter ;
+    private double Easting = 0;
+    private double Northing = 0;
 
 
     ImageView imageView;
@@ -74,10 +85,12 @@ public class CameraActivity extends AppCompatActivity {
         setContentView(R.layout.activity_camera);
 
         imageView = (ImageView) findViewById(R.id.cameraImage);
+        fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
 
 
         Intent cameraIntent = new Intent();
         cameraIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        tv5 = (TextView) findViewById(R.id.textView5);
 
 
         //Uri outputUri=FileProvider.getUriForFile(getActivity().getApplicationContext(), AUTHORITY, imageFile);
@@ -88,28 +101,63 @@ public class CameraActivity extends AppCompatActivity {
         startActivityForResult(cameraIntent, CAMERA_REQUEST);
 
 
+
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+
             return;
         }
-        Location location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        mlocation = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
 
-        if(location != null && location.getTime() > Calendar.getInstance().getTimeInMillis() - 2 * 60 * 1000) {
-            // Do something with the recent location fix
-            //  otherwise wait for the update below
-            tv5 = (TextView) findViewById(R.id.textView5);
-            tv5.setText("Lokacija je"+location.getLatitude() + location.getLongitude());
+
+        if (mlocation != null) {
+            tv5.setText(demo(mlocation.getLatitude(), mlocation.getLongitude()));
         }
         else {
-            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
+            tv5.setText("Current Location: No Data");
         }
+
+        // tv.setText(demo(mlocation.getLatitude(),mlocation.getLongitude()));
+
+
+
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+
+
+        mlocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                tv5.setText(demo(mlocation.getLatitude(),mlocation.getLongitude()));
+                locationManager.removeUpdates(this);
+
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent mi = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(mi);
+            }
+        };
+
+        configure_button();
+
 
 
 
@@ -188,6 +236,99 @@ public class CameraActivity extends AppCompatActivity {
         }
     }
 
+
+    void configure_button(){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        // Code to manually refresh location
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //noinspection MissingPermission
+                locationManager.requestLocationUpdates("gps", 5000, 0, mlocationListener);
+                Log.i("LISTENER", "RADIIIIIIII");
+
+
+            }
+        });
+    }
+
+
+    // to get Easting and Northing
+
+    private String demo(double Lat, double Lon)
+    {
+        Zone= (int) Math.floor(Lon/6+31);
+
+        System.out.println("Zone"+Zone);
+        if (Lat<-72)
+            Letter='C';
+        else if (Lat<-64)
+            Letter='D';
+        else if (Lat<-56)
+            Letter='E';
+        else if (Lat<-48)
+            Letter='F';
+        else if (Lat<-40)
+            Letter='G';
+        else if (Lat<-32)
+            Letter='H';
+        else if (Lat<-24)
+            Letter='J';
+        else if (Lat<-16)
+            Letter='K';
+        else if (Lat<-8)
+            Letter='L';
+        else if (Lat<0)
+            Letter='M';
+        else if (Lat<8)
+            Letter='N';
+        else if (Lat<16)
+            Letter='P';
+        else if (Lat<24)
+            Letter='Q';
+        else if (Lat<32)
+            Letter='R';
+        else if (Lat<40)
+            Letter='S';
+        else if (Lat<48)
+            Letter='T';
+        else if (Lat<56)
+            Letter='U';
+        else if (Lat<64)
+            Letter='V';
+        else if (Lat<72)
+            Letter='W';
+        else
+            Letter='X';
+        Easting=0.5*Math.log((1+Math.cos(Lat*Math.PI/180)*Math.sin(Lon*Math.PI/180-(6*Zone-183)*Math.PI/180))/(1-Math.cos(Lat*Math.PI/180)*Math.sin(Lon*Math.PI/180-(6*Zone-183)*Math.PI/180)))*0.9996*6399593.62/Math.pow((1+Math.pow(0.0820944379, 2)*Math.pow(Math.cos(Lat*Math.PI/180), 2)), 0.5)*(1+ Math.pow(0.0820944379,2)/2*Math.pow((0.5*Math.log((1+Math.cos(Lat*Math.PI/180)*Math.sin(Lon*Math.PI/180-(6*Zone-183)*Math.PI/180))/(1-Math.cos(Lat*Math.PI/180)*Math.sin(Lon*Math.PI/180-(6*Zone-183)*Math.PI/180)))),2)*Math.pow(Math.cos(Lat*Math.PI/180),2)/3)+500000;
+        Easting=Math.round(Easting*100)*0.01;
+
+
+        Northing = (Math.atan(Math.tan(Lat*Math.PI/180)/Math.cos((Lon*Math.PI/180-(6*Zone -183)*Math.PI/180)))-Lat*Math.PI/180)*0.9996*6399593.625/Math.sqrt(1+0.006739496742*Math.pow(Math.cos(Lat*Math.PI/180),2))*(1+0.006739496742/2*Math.pow(0.5*Math.log((1+Math.cos(Lat*Math.PI/180)*Math.sin((Lon*Math.PI/180-(6*Zone -183)*Math.PI/180)))/(1-Math.cos(Lat*Math.PI/180)*Math.sin((Lon*Math.PI/180-(6*Zone -183)*Math.PI/180)))),2)*Math.pow(Math.cos(Lat*Math.PI/180),2))+0.9996*6399593.625*(Lat*Math.PI/180-0.005054622556*(Lat*Math.PI/180+Math.sin(2*Lat*Math.PI/180)/2)+4.258201531e-05*(3*(Lat*Math.PI/180+Math.sin(2*Lat*Math.PI/180)/2)+Math.sin(2*Lat*Math.PI/180)*Math.pow(Math.cos(Lat*Math.PI/180),2))/4-1.674057895e-07*(5*(3*(Lat*Math.PI/180+Math.sin(2*Lat*Math.PI/180)/2)+Math.sin(2*Lat*Math.PI/180)*Math.pow(Math.cos(Lat*Math.PI/180),2))/4+Math.sin(2*Lat*Math.PI/180)*Math.pow(Math.cos(Lat*Math.PI/180),2)*Math.pow(Math.cos(Lat*Math.PI/180),2))/3);
+        if (Letter<'M')
+            Northing = Northing + 10000000;
+
+
+        Northing=Math.round(Northing*100)*0.01;
+
+        String s = "Easting: "+Easting + "\nNorthing: "+ Northing;
+
+        return s;
+
+
+
+
+    }
 
     @Override
     public void onBackPressed() {
