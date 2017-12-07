f1fdac9b1e89c7fd5e9fe0addf60d3524b32d4d6
DamjanJ <damjanjelic@gmail.com>
2017-12-07 13:29:57 +0100

    Commit 2, Login credentials remembered

--- app/src/main/java/rs/org/habiprot/alciphron/alci/GalleryActivityTwo.java
+++ app/src/main/java/rs/org/habiprot/alciphron/alci/GalleryActivityTwo.java
@@ -1,15 +1,27 @@
 package rs.org.habiprot.alciphron.alci;
+// com.bbn.openmap.proj.coords; MGRS koordinate se ovde konvertuju
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.Image;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
@@ -22,6 +34,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -36,10 +50,19 @@ import java.util.Date;
  */
 
 
-
 public class GalleryActivityTwo extends AppCompatActivity {
 
     private static int RESULT_LOAD_IMAGE = 1;
+    private LocationListener mlocationListener;
+    private LocationManager locationManager;
+    private FloatingActionButton fab;
+    private TextView tv;
+    private Location mlocation;
+
+    private int Zone = 0;
+    private char Letter ;
+    private double Easting = 0;
+    private double Northing = 0;
 
 
     @Override
@@ -54,8 +77,89 @@ public class GalleryActivityTwo extends AppCompatActivity {
 
         startActivityForResult(i, RESULT_LOAD_IMAGE);
 
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-    }
+            return;
+        }
+        mlocation = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        tv = (TextView) findViewById(R.id.textView2);
+
+        if (mlocation != null) {
+            tv.setText(demo(mlocation.getLatitude(), mlocation.getLongitude()));
+        }
+        else {
+            tv.setText("Current Location: No Data");
+        }
+
+       // tv.setText(demo(mlocation.getLatitude(),mlocation.getLongitude()));
+
+
+
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+
+
+        mlocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                tv.setText(demo(mlocation.getLatitude(),mlocation.getLongitude()));
+                locationManager.removeUpdates(this);
+
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent mi = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(mi);
+            }
+        };
+
+        configure_button();
+
+
+
+
+
+
+/*
+
+
+
+        // getLocationButton is the name of your button.  Not the best name, I know.
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // instantiate the location manager, note you will need to request permissions in your manifest
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                // get the last know location from your location manager.
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                // now get the lat/lon from the location and do something with it.
+
+                Log.i("duzina "+location.getLatitude(), "sirina "  + location.getLongitude());
+            }
+        });
+*/
+}
+
+
+
 
 
     @Override
@@ -119,5 +223,113 @@ public class GalleryActivityTwo extends AppCompatActivity {
         super.onBackPressed();
         this.finish();
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void configure_button(){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        // Code to manually refresh location
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //noinspection MissingPermission
+                locationManager.requestLocationUpdates("gps", 5000, 0, mlocationListener);
+                Log.i("LISTENER", "RADIIIIIIII");
+
+
+            }
+        });
+    }
+
+
+
+    private String demo(double Lat, double Lon)
+    {
+        Zone= (int) Math.floor(Lon/6+31);
+
+        System.out.println("Zone"+Zone);
+        if (Lat<-72)
+            Letter='C';
+        else if (Lat<-64)
+            Letter='D';
+        else if (Lat<-56)
+            Letter='E';
+        else if (Lat<-48)
+            Letter='F';
+        else if (Lat<-40)
+            Letter='G';
+        else if (Lat<-32)
+            Letter='H';
+        else if (Lat<-24)
+            Letter='J';
+        else if (Lat<-16)
+            Letter='K';
+        else if (Lat<-8)
+            Letter='L';
+        else if (Lat<0)
+            Letter='M';
+        else if (Lat<8)
+            Letter='N';
+        else if (Lat<16)
+            Letter='P';
+        else if (Lat<24)
+            Letter='Q';
+        else if (Lat<32)
+            Letter='R';
+        else if (Lat<40)
+            Letter='S';
+        else if (Lat<48)
+            Letter='T';
+        else if (Lat<56)
+            Letter='U';
+        else if (Lat<64)
+            Letter='V';
+        else if (Lat<72)
+            Letter='W';
+        else
+            Letter='X';
+        Easting=0.5*Math.log((1+Math.cos(Lat*Math.PI/180)*Math.sin(Lon*Math.PI/180-(6*Zone-183)*Math.PI/180))/(1-Math.cos(Lat*Math.PI/180)*Math.sin(Lon*Math.PI/180-(6*Zone-183)*Math.PI/180)))*0.9996*6399593.62/Math.pow((1+Math.pow(0.0820944379, 2)*Math.pow(Math.cos(Lat*Math.PI/180), 2)), 0.5)*(1+ Math.pow(0.0820944379,2)/2*Math.pow((0.5*Math.log((1+Math.cos(Lat*Math.PI/180)*Math.sin(Lon*Math.PI/180-(6*Zone-183)*Math.PI/180))/(1-Math.cos(Lat*Math.PI/180)*Math.sin(Lon*Math.PI/180-(6*Zone-183)*Math.PI/180)))),2)*Math.pow(Math.cos(Lat*Math.PI/180),2)/3)+500000;
+        Easting=Math.round(Easting*100)*0.01;
+
+
+        Northing = (Math.atan(Math.tan(Lat*Math.PI/180)/Math.cos((Lon*Math.PI/180-(6*Zone -183)*Math.PI/180)))-Lat*Math.PI/180)*0.9996*6399593.625/Math.sqrt(1+0.006739496742*Math.pow(Math.cos(Lat*Math.PI/180),2))*(1+0.006739496742/2*Math.pow(0.5*Math.log((1+Math.cos(Lat*Math.PI/180)*Math.sin((Lon*Math.PI/180-(6*Zone -183)*Math.PI/180)))/(1-Math.cos(Lat*Math.PI/180)*Math.sin((Lon*Math.PI/180-(6*Zone -183)*Math.PI/180)))),2)*Math.pow(Math.cos(Lat*Math.PI/180),2))+0.9996*6399593.625*(Lat*Math.PI/180-0.005054622556*(Lat*Math.PI/180+Math.sin(2*Lat*Math.PI/180)/2)+4.258201531e-05*(3*(Lat*Math.PI/180+Math.sin(2*Lat*Math.PI/180)/2)+Math.sin(2*Lat*Math.PI/180)*Math.pow(Math.cos(Lat*Math.PI/180),2))/4-1.674057895e-07*(5*(3*(Lat*Math.PI/180+Math.sin(2*Lat*Math.PI/180)/2)+Math.sin(2*Lat*Math.PI/180)*Math.pow(Math.cos(Lat*Math.PI/180),2))/4+Math.sin(2*Lat*Math.PI/180)*Math.pow(Math.cos(Lat*Math.PI/180),2)*Math.pow(Math.cos(Lat*Math.PI/180),2))/3);
+        if (Letter<'M')
+            Northing = Northing + 10000000;
+
+
+        Northing=Math.round(Northing*100)*0.01;
+
+        String s = "Easting: "+Easting + "\nNorthing: "+ Northing;
+
+        return s;
+
+
+
+
+    }
+
+
+
+
 }
 
--- app/src/main/java/rs/org/habiprot/alciphron/alci/LoginActivity.java
+++ app/src/main/java/rs/org/habiprot/alciphron/alci/LoginActivity.java
@@ -3,7 +3,9 @@ package rs.org.habiprot.alciphron.alci;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -24,9 +26,11 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -62,11 +66,26 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+    private CheckBox saveLoginCheckBox;
+    private SharedPreferences loginPreferences;
+    private SharedPreferences.Editor loginPrefsEditor;
+    private Boolean saveLogin;
+    private String musername,mpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        saveLoginCheckBox = (CheckBox)findViewById(R.id.checkBox);
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
+
+
+
+
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
@@ -93,6 +112,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        if (saveLogin == true) {
+            mEmailView.setText(loginPreferences.getString("username", ""));
+            mPasswordView.setText(loginPreferences.getString("password", ""));
+            saveLoginCheckBox.setChecked(true);
+        }
     }
 
     private void populateAutoComplete() {
@@ -159,6 +184,27 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         boolean cancel = false;
         View focusView = null;
+
+        // custom <code></code>
+
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(mEmailView.getWindowToken(), 0);
+
+        musername = mEmailView.getText().toString();
+        mpassword = mPasswordView.getText().toString();
+
+        if (saveLoginCheckBox.isChecked()) {
+            loginPrefsEditor.putBoolean("saveLogin", true);
+            loginPrefsEditor.putString("username", musername);
+            loginPrefsEditor.putString("password", mpassword);
+            loginPrefsEditor.commit();
+        } else {
+            loginPrefsEditor.clear();
+            loginPrefsEditor.commit();
+        }
+
+
+        //custom <code></code>
 
         // Check for a valid password, if the user entered one.
         if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
